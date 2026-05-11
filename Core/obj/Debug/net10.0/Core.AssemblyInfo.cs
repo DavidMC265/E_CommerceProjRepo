97f1e024665b4c3ebe8e2e091dd8947558790925
DavidKaos26 <da.mayorga1305@gmail.com>
2026-05-11 11:09:04 -0600

    API Basics Integration

--- Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf0988f575b2630ef7365b3aee8792b9189ec36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
